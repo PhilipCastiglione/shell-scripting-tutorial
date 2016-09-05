365ae7d0c47c1b2aa738692180c64538adf3ff5c
Philip Castiglione <philipcastiglione@gmail.com>
2016-09-05 14:34:01 +1000

    basic command control flow

---
+++ basic-control.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo a really long line that we want to split for \
+     aesthetic reasons
+
+echo effectively ; echo multiple ; echo lines
+
+echo any && echo failures && ls cancel && echo following commands
+
+ls failure || echo is skipped until something works || echo then all else is ignored
