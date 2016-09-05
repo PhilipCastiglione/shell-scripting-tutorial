acdc17d5081b3c03b21146a88118966a54b5b128
Philip Castiglione <philipcastiglione@gmail.com>
2016-09-05 16:32:06 +1000

    nested if, elif

--- if-control.sh
+++ if-control.sh
@@ -5,3 +5,22 @@ then
 else
   echo "you ran this script with these args: $*"
 fi
+
+if [ $# -eq 1 ]
+then
+  echo "this is the single arg you provided: $1"
+elif [ $# -gt 1 ]
+then
+  echo "this is the second arg you provided: $2"
+else
+  echo "try again with some args"
+fi
+
+if [ $# -gt 2 ]
+then
+  echo "this is the third arg: $3"
+  if [ $# -gt 3 ]
+  then
+    echo "this is the fourth izzle: $4"
+  fi
+fi
